0594d0607ec49b3e7381dee7316acb5288e97777
NumberI <x0987654321@ya.ru>
2015-11-18 13:34:11 +0300

    add index page and routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
 class PostsController < ApplicationController
+  def index
+    @images = Image.all
+    @links = Link.all
+    @articles = Post.all
+    @posts = (@images + @links + @articles).sort_by {|x| x.created_at}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  root to: "posts#index" 
+  resources :posts do
+    resources :comments
+  end
+  resource :images, only: [:new, :create], path_names: {:new => ""}
+  resource :links, only: [:new, :create], path_names: {:new => ""}
+  resource :posts, only: [:new, :create], path_names: {:new => ""}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
